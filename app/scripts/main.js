d4786edb62fdf488412920ae1512fd12ee76c382
benjah1 <benjaminhuang1@gmail.com>
2015-01-05 14:34:04 -0500

    Editor prototype

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,49 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+var App = angular.module('ppt', []);
+
+App.directive('liveEditor', [ function() {
+
+	console.log('hello world');
+
+	var template = '\
+		<div class="cssEditor">\
+			<textarea ng-model="cssEditor"></textarea>\
+		</div>\
+		<div class="codeEditor">\
+			<textarea ng-model="codeEditor"></textarea>\
+		</div>\
+		<div class="preview">\
+			<iframe></iframe>\
+		</div>\
+		';
+
+	var link = function ($scope, element) {
+		var body = jQuery(element).find('iframe').contents().find('body');
+		body.html('<style id="styler"></style><div id="container"></div>');
+
+		var styler = body.find('#styler');
+		var container = body.find('#container');
+
+		$scope.cssEditor = $scope.css;
+		$scope.codeEditor = $scope.code;
+
+		$scope.$watch('cssEditor', function(val){
+			styler.html(val);
+		});
+
+		$scope.$watch('codeEditor', function(val){
+			container.html(val);
+		});
+	};
+	
+	return {
+		restrict: 'A',
+		//transclude: false,
+		link: link,
+		scope: {
+			css: '@css',
+			code: '@code'
+		},
+    template: template
+	};
+
+}]);
